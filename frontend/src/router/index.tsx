abff3e66c37fe5196f4efdd21d8fe156e78d4501
Kanatov <ilya.kanatov@gmail.com>
2024-11-03 14:40:45 +0000

    - fixing build errors;- bundle chunking optimisation;

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,16 +1,20 @@
-import SymbolsView from '@/components/SymbolsView';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import StatementsView from '@/components/StatementsView';
-import ProfileView from '@/components/ProfileView';
+import { Suspense, lazy } from 'react';
+const SymbolsView = lazy(() => import('@/components/SymbolsView'));
+const ProfileView = lazy(() => import('@/components/ProfileView'));
+const StatementsView = lazy(() => import('@/components/StatementsView'));
 
 const Router = () => {
   return (
-    <Routes>
-      <Route index element={<SymbolsView />} />
-      <Route index path="profile" element={<ProfileView />} />
-      <Route index path="statements" element={<StatementsView />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    // TODO: add fallback
+    <Suspense fallback={<h3>Loading app...</h3>}>
+      <Routes>
+        <Route index element={<SymbolsView />} />
+        <Route index path="profile" element={<ProfileView />} />
+        <Route index path="statements" element={<StatementsView />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
   );
 };
 
